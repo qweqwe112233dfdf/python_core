770a9fa6449aab668b123087bcdae7dc08038c16
qweqwe112233dfdf <radchenkodmytro09@gmail.oom>
2025-12-19 11:54:00 +0200

    first commit

--- dz1.py
+++ dz1.py
@@ -1,1 +1,3 @@
-print
+print("Мене звати Дмитро, мені 18 років, вчусь в одеській політехніці.")
+print("Мої 5 улюблених ігор: rust, counter-strike, dota, pubg, mafia 2.")
+print("Мої 5 улюблених фільмів: Гран-прі, серіал Prison Break, серіал Wayne, серіал Looking for Alaska, Home Alone")
