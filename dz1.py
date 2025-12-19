d965d0bcfc3ee4ad30e2ef0b4f3e6a9233fa5e9b
qweqwe112233dfdf <radchenkodmytro09@gmail.oom>
2025-12-19 12:03:13 +0200

    1

--- dz1.py
+++ dz1.py
@@ -1,3 +1,4 @@
 print("Мене звати Дмитро, мені 18 років, вчусь в одеській політехніці.")
 print("Мої 5 улюблених ігор: rust, counter-strike, dota, pubg, mafia 2.")
 print("Мої 5 улюблених фільмів: Гран-прі, серіал Prison Break, серіал Wayne, серіал Looking for Alaska, Home Alone")
+print("Живу життя, граю в ігри, дивлюсь серіали")
